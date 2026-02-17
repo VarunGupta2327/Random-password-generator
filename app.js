5b3b40450345e1c8c7969ae4979d415cca2edb7f
Varun <83004941+VarunGupta2327@users.noreply.github.com>
2026-02-17 14:27:46 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,49 @@
+const upperSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerSet = "abcdefghijklmnopqrstuvwxyz";
+const numberSet = "1234567890";
+const symbolSet = "~!@#$%^&*()_+/";
+
+//Selectors
+const passBox = document.getElementById("pass-box");
+const totalChar = document.getElementById("total-char");
+const upperInput = document.getElementById("upper-case");
+const lowerInput = document.getElementById("lower-case");
+const numberInput = document.getElementById("numbers");
+const symbolInput = document.getElementById("symbols");
+
+const getRandomData = (dataSet) => {
+  return dataSet[Math.floor(Math.random() * dataSet.length)];
+};
+
+const generatePassword = (password = "") => {
+  if (upperInput.checked) {
+    password += getRandomData(upperSet);
+  }
+  if (lowerInput.checked) {
+    password += getRandomData(lowerSet);
+  }
+  if (numberInput.checked) {
+    password += getRandomData(numberSet);
+  }
+  if (symbolInput.checked) {
+    password += getRandomData(symbolSet);
+  }
+
+  if (password.length < totalChar.value) {
+    return generatePassword(password);
+    // it make more than required
+  }
+  passBox.innerText = truncateString(password, totalChar.value);
+};
+
+generatePassword();
+
+// this trim the value at which it is required
+function truncateString(str, num) {
+  if (str.length > num) {
+    let subStr = str.substring(0, num); // extract value from 0 index to num index
+    return subStr;
+  } else {
+    return str;
+  }
+}
